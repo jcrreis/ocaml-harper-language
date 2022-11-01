2e1290f58dbec6dc956627775c456f09eab1f356
João Reis <jcr.reis@campus.fct.unl.pt>
2022-11-01 19:40:18 +0000

    update last commit

--- main.ml
+++ main.ml
@@ -64,8 +64,8 @@ let rec free_variables (e: expr) : string list = match e with
   | Div (e1, e2) -> free_variables e1 @ free_variables e2
   | Cat (e1, e2) -> free_variables e1 @ free_variables e2
   | Len (e1) -> free_variables e1 
-  | Let (x, e1, e2) -> List.filter (fun (k') -> x <> k') (free_variables e1 @ free_variables e2)
-  | F_def (_, _, _, x, e1) -> List.filter (fun (k') -> x <> k') (free_variables e1)
+  | Let (x, e1, e2) -> List.filter (fun (x') -> x <> x') (free_variables e1 @ free_variables e2)
+  | F_def (_, _, _, x, e1) -> List.filter (fun (x') -> x <> x') (free_variables e1)
   | F_apply (_, e1) -> []
 
 let rec substitute (e: expr) (v: expr) (x: string) : expr = match e with
