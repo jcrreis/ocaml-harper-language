0f8d1b9b20bac0b4e0acb912b54e878b778528ab
João Reis <jcr.reis@campus.fct.unl.pt>
2022-11-27 11:00:56 +0000

    added more to grammar

--- fs.ml
+++ fs.ml
@@ -1,5 +1,6 @@
 (* BEFORE 0.5.0 there was no distinction between address and address payable!!! *)
 (* msg.sender.transfer(x) to payable(msg.sender).transfer(x) *)
+
 type t_exp = 
   | C of string (* * hash_contract_code? *)
   | Bool
@@ -21,20 +22,28 @@ type values =
 type expr =
   | Var of string
   | Val of values
-  | This of string (* * string (? contracto a que se refer o this) *)
+  | This 
   | MsgSender 
   | MsgValue 
   | Balance of expr
   | Address of expr 
-  | Assign of string * expr
+  | StateRead of expr * string
   | Transfer of expr * expr
-  | Let of string * expr * expr
   | New of string * expr (* list of expr ? parameters *)
-  | Revert 
-  | If of expr * expr * expr 
+  | Cons of string * expr 
   | Seq of expr * expr
+  | Let of (*t_exp * ?*) string * expr * expr (* EM SOLIDITY NÃO EXISTE *)
+  | Assign of string * expr
+  | StateAssign of expr * string * expr
+  | MapRead of expr * expr 
+  | MapWrite of expr * expr * expr
+  | Call of expr * string * expr * expr (*List?*) 
+  | CallVariant of expr * string * expr * expr * expr (*list?*) 
+  | Revert
+  | If of expr * expr * expr 
   | Return of expr
 and arit_ops = 
+  | Num of int
   | Plus of expr * expr 
   | Div of expr * expr 
   | Times of expr * expr
@@ -42,6 +51,7 @@ and arit_ops =
   | Exp of expr * expr 
   | Mod of expr * expr 
 and bool_ops =
+  | Bool of b_val
   | Neg of b_val
   | Conj of b_val * b_val
   | Disj of b_val * b_val
@@ -64,14 +74,37 @@ type contract_def =
   | Constructor of t_exp * string (* list *) * expr 
   | Functions of fun_def (*list*)
 
+let ct: (string, contract_def) Hashtbl.t = Hashtbl.create 64
+
+
+let rec decompose_arit_expr (e: arit_ops) : arit_ops = match e with
+  (* | Plus (e1, e2) -> begin match e1, e2 with
+    | Num n1, Num n2 -> Num (n1 + n2)
+    | Num n1, e2 -> eval_expr (Plus(Num(n1), eval_expr e2))
+    | e1, e2 -> eval_expr Plus(eval_expr e1, e2)
+    end   *)
+  | _ -> assert false
+
+let rec decompose_bool_expr (e: bool_ops) : bool_ops = match e with
+  | Neg (b1) -> begin match b1 with 
+    | False -> Bool(True)
+    | True -> Bool(False)
+    end
+  | Conj (b1, b2) -> begin match b1, b2 with
+    | False, _ -> Bool(False) 
+    | _, False -> Bool(False)
+    | True, True -> Bool(True)
+    end  
+  | Disj (b1, b2) -> begin match b1, b2 with
+    | True, _ -> Bool(True)
+    | _, True -> Bool(True)
+    | False, False -> Bool(False) 
+    end
+  | _ -> assert false
 
 let rec eval_expr (e: expr) : expr = match e with
-  | Plus (e1, e2) -> begin match e1, e2 with
-    | Val(VUInt(n1)), Val(VUInt(n2)) -> Val(VUInt(n1 + n2))
-    | Val(VUInt(n1)), e2 -> eval_expr (Plus(Val(VUInt(n1)), eval_expr e2))
-    | e1, e2 -> eval_expr Plus(eval_expr e1, e2)
-    end  
-  | _ -> assert false
+	| Var(x) -> Var(x)
+	| _ -> assert false
 
 
 
